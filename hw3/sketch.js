eb18a5d8aa306f4a260c241865d9c9394c8a1348
navinad-create18 <navina@vt.edu>
2026-02-08 23:50:24 -0500

    Worked on HW 3

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,4 +1,11 @@
-let dino;
+let flamingo;
+let myBubbles;
+
+let bubbleColors = [
+    [19, 76, 145,70],//dark blue
+    [76, 149, 237,80],//medium blue
+    [204, 227, 255,75]//light blue
+];
 // fourth value in color for transparency or alpha value
 
 
@@ -8,47 +15,123 @@ function setup(){
     
     canvas.parent('sketch-holder');
     
-    dino = createDino();
+    flamingo = createFlamingo();
+    myBubbles = blueBubbles();
 }
 
 function draw(){
-    background(204, 240, 217);
+    background(194, 155, 250);
+    
     
     orbitControl();
     lights();
     noStroke();
     
+    push();
+    translate(20,50,0);
+    emissiveMaterial(30,40,40);
+    specularMaterial(13, 190, 255);
+    shininess(100);
+    model(flamingo);
+    pop();
     
-    model(dino);
+    model(myBubbles);
 
 }
 
-function createDino(){
+function createFlamingo(){
     beginGeometry();
     
+    //main body
     push();
-    fill(10, 79, 35);
-    box(150,80,150);
-    translate(0,-30,60);
-    ellipsoid(80,30,30);
-    translate(0,60,0);
-    ellipsoid(80,30,30);
-    translate(0,-30,0);
-    ellipsoid(80,30,30);
+    rotateX(90);
+    fill(255, 59, 163);
+    torus(100,50);
     pop();
     
+    //neck
     push();
-    fill(10, 79, 35);
-    translate(-70,0,70);
-    ellipsoid(20,45,20);
-    translate(140,0,0);
-    ellipsoid(20,45,20);
+    fill(255, 59, 163);
+    translate(-90,-20,0);
+    cylinder(30,40,30);
+    translate(0,-20,0);
+    ellipsoid(30,50,30);
+    translate(20,-40,0);
+    rotateZ(35);
+    ellipsoid(30,50,30);
+    translate(-10,-40,0);
+    rotateZ(-35);
+    ellipsoid(30,50,30);
+    translate(-10,-40,0);
+    rotateZ(-35);
+    ellipsoid(30,50,30);
+    translate(-10,-40,0);
+    rotateZ(-35);
+    ellipsoid(30,50,30);
+    translate(-10,-40,0);
+    rotateZ(-35);
+    ellipsoid(30,50,30);
+    translate(-10,-40,0);
+    rotateZ(-35);
+    fill(255, 224, 191);
+    ellipsoid(15,40,15);
+    translate(-15,-50,0);
+    rotateZ(-45);
+    fill(0);
+    ellipsoid(12,30,12);
     pop();
-
+    
+    //front left eye
+    push();
+    translate(-150,-185,15);
+    fill(255);
+    sphere(17,15,20);
+    translate(-2,1,9);
+    fill(0);
+    sphere(10,10,10);
+    translate(1,0,6);
+    fill(255);
+    sphere(5,5,5);
+    pop();
+    
+    //back right eye
+    push();
+    translate(-150,-185,-15);
+    fill(255);
+    sphere(17,15,20);
+    translate(-2,1,-9);
+    fill(0);
+    sphere(10,10,10);
+    translate(1,0,-6);
+    fill(255);
+    sphere(5,5,5);
+    pop();
     
     
     
     let shape = endGeometry();
     return shape;
     
+}
+
+function blueBubbles(){
+    beginGeometry();
+       for (let i = 0; i < 20; i++) {
+           translate(-10,10,-10);
+    let x = random(0,200);
+    let y = random(50,130);
+    let z = random(150);
+    let c = random(bubbleColors);
+    let s = random(0.5,1.5);
+    
+    push();
+    noStroke();
+    translate(x,y,z);
+    fill(...c);
+    scale(s);
+    sphere(20,20,20);
+    pop();
+       }
+    let shape = endGeometry();
+    return shape;
 }

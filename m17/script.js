2c321bd01dc1055729248518aeb7a965ae36b0aa
navinad-create18 <navina@vt.edu>
2026-03-17 16:33:09 -0400

    m17 updated

--- m17/script.js
+++ m17/script.js
@@ -11,8 +11,24 @@ import * as THREE from 'three';
 // The plug-in for orbit controls
 import { OrbitControls } from './src/OrbitControls.js';
 
+//The plug-in for First Person Controls
+import { PointerLockControls } from './src/PointerLockControls.js';
+
 // Declaring global variables.
 let camera, canvas, controls, scene, renderer;
+
+//Variables for First Person Controls
+let raycaster;
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
 
 // Run the "init" function which is like "setup" in p5.
 init();
@@ -27,24 +43,123 @@ function init() {
     scene.fog = new THREE.FogExp2( 0xbfeff5, 0.0015 );
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     //renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( 400,400 );
+    renderer.setSize( innerWidth, innerHeight );
     renderer.setAnimationLoop( animate );
     canvas.appendChild( renderer.domElement );
 
     // Setup camera
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 400, 200, 0 );
+    camera.position.set( 0, 10, 0 );
 
-    // Setup controls
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.listenToKeyEvents( window ); 
-    controls.enableDamping = true; 
-    controls.dampingFactor = 0.05;
-    controls.screenSpacePanning = false;
-    controls.minDistance = 100;
-    controls.maxDistance = 500;
-    controls.cursorStyle = 'grab';
-    controls.maxPolarAngle = Math.PI / 2;
+    // Setup Orbit controls
+    //controls = new OrbitControls( camera, renderer.domElement );
+    //controls.listenToKeyEvents( window ); 
+    //controls.enableDamping = true; 
+    //controls.dampingFactor = 0.05;
+    //controls.screenSpacePanning = false;
+    //controls.minDistance = 100;
+    //controls.maxDistance = 500;
+    //controls.cursorStyle = 'grab';
+    //controls.maxPolarAngle = Math.PI / 2;
+    
+    // Setup First Person Controls
+    controls = new PointerLockControls( camera, document.body );
+
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+
+    instructions.addEventListener( 'click', function () {
+controls.lock();
+        
+        controls.lock();
+
+    } );
+
+    controls.addEventListener( 'lock', function () {
+
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+
+    } );
+
+    controls.addEventListener( 'unlock', function () {
+
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+
+    } );
+
+    scene.add( controls.object );
+
+    const onKeyDown = function ( event ) {
+
+        switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+
+        case 'Space':
+            if ( canJump === true ) velocity.y += 350;
+            canJump = false;
+            break;
+
+        }
+
+    };
+
+    const onKeyUp = function ( event ) {
+
+    switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+
+        }
+
+    };
+
+    document.addEventListener( 'keydown', onKeyDown );
+    document.addEventListener( 'keyup', onKeyUp );
+
+    raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+				
+
+    //End of First person setup
 
     // Add world geometry
 
@@ -86,8 +201,39 @@ function init() {
 // Function to update moving objects, in this case the camera.
 // The render function is trigger at the end to update the canvas.
 function animate() {
-    controls.update();
-    render();
+    
+    //Start First Person Control Animations
+    const time = performance.now();
+    if ( controls.isLocked === true ) {
+
+        
+        const delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+
+        controls.moveRight( - velocity.x * delta );
+        controls.moveForward( - velocity.z * delta );
+
+            canJump = true;
+
+    }
+
+    prevTime = time;
+
+    //End First Person Control Animations
+    
+    renderer.render( scene, camera );
 }
 
 // Function to render the scene using the camera.

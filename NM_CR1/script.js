d08fa2fabc114ab4cbe1bf1e23c22e2f991c49cf
navinad-create18 <navina@vt.edu>
2026-03-23 12:33:21 -0400

    Added photos and completed CR 1

--- NM_CR1/script.js
+++ NM_CR1/script.js
@@ -185,15 +185,15 @@ controls.lock();
         loader.load( './assets/TreeHouseScape2.glb',  function ( gltf ) {
 
             hills = gltf.scene;
-            hills.scale.set (20,20,20);
-            hills.position.set(0,0,-50);
+            hills.scale.set (40,40,40);
+            hills.position.set(0,0,-80);
             scene.add( hills ); 
 
             loader.load( './assets/branchesJump.glb',  function ( gltf ) {
 
             branches = gltf.scene;
-            branches.scale.set (20,20,20);
-            branches.position.set(0,0,-50);
+            branches.scale.set (40,40,40);
+            branches.position.set(0,0,-80);
             
             scene.add( branches ); 
             objects.push(branches);
@@ -206,26 +206,72 @@ controls.lock();
         } );
     
     //house floor
-    const treeHouse = new THREE.BoxGeometry( 10, 16,1 );
-    const houseFloor = new THREE.MeshPhongMaterial( { color: 0x402314, flatShading: true } );
-    const mesh3 = new THREE.Mesh( treeHouse, houseFloor, 10 );
-    mesh3.position.set(-5,35,-54);
+    const treeHouse1 = new THREE.BoxGeometry( 20, 31,0.5 );
+    const houseFloor1 = new THREE.MeshPhongMaterial( { color: 0x402314, flatShading: true } );
+    const mesh3 = new THREE.Mesh( treeHouse1, houseFloor1, 10 );
+    mesh3.position.set(-14,66,-90);
     mesh3.rotateX( -1.5708 );
     scene.add( mesh3 );
+    objects.push(mesh3);
+    
+    const treeHouse2 = new THREE.BoxGeometry( 16, 31,0.5 );
+    const houseFloor2 = new THREE.MeshPhongMaterial( { color: 0x402314, flatShading: true } );
+    const mesh4 = new THREE.Mesh( treeHouse2, houseFloor2, 10 );
+    mesh4.position.set(17,66,-90);
+    mesh4.rotateX( -1.5708 );
+    scene.add( mesh4 );
+    objects.push(mesh4);
+    
+    const treeHouse3 = new THREE.BoxGeometry( 20, 13,0.5 );
+    const houseFloor3 = new THREE.MeshPhongMaterial( { color: 0x402314, flatShading: true } );
+    const mesh5 = new THREE.Mesh( treeHouse3, houseFloor3, 10 );
+    mesh5.position.set(0,66,-98);
+    mesh5.rotateX( -1.5708 );
+    scene.add( mesh5 );
+    objects.push(mesh5);
+    
+    const treeHouse4 = new THREE.BoxGeometry( 20, 12,0.5 );
+    const houseFloor4 = new THREE.MeshPhongMaterial( { color: 0x402314, flatShading: true } );
+    const mesh6 = new THREE.Mesh( treeHouse4, houseFloor4, 10 );
+    mesh6.position.set(0,66,-78);
+    mesh6.rotateX( -1.5708 );
+    scene.add( mesh6 );
+    objects.push(mesh6);
+    
+    //loading photos
+    const texture1 = new THREE.TextureLoader().load( './assets/Flower1.JPG' );
+    const texture2 = new THREE.TextureLoader().load( './assets/Flower2.JPG' );
+    const texture3 = new THREE.TextureLoader().load( './assets/Flower3.JPG' );
+    const texture4 = new THREE.TextureLoader().load( './assets/Flower4.JPG' );
+    
+    //photos
+    const photo1 = new THREE.PlaneGeometry( 10, 13 );
+    const frame1 = new THREE.MeshBasicMaterial( { map: texture1 } );
+    const mesh7 = new THREE.Mesh( photo1,frame1, 10 );
+    mesh7.position.set(10,75,-73);
+    mesh7.rotateY( 3.14159 );
+    scene.add( mesh7 );
+    
+    const photo2 = new THREE.PlaneGeometry( 10, 13 );
+    const frame2 = new THREE.MeshBasicMaterial( { map: texture2 } );
+    const mesh8 = new THREE.Mesh( photo2,frame2, 10 );
+    mesh8.position.set(0,80,-103);
+    //mesh8.rotateY( 3.14159 );
+    scene.add( mesh8 );
+    
+    const photo3 = new THREE.PlaneGeometry( 10, 13 );
+    const frame3 = new THREE.MeshBasicMaterial( { map: texture3 } );
+    const mesh9 = new THREE.Mesh( photo3,frame3, 10 );
+    mesh9.position.set(15,80,-103);
+    scene.add( mesh9 );
+    
+    const photo4 = new THREE.PlaneGeometry( 10, 13 );
+    const frame4 = new THREE.MeshBasicMaterial( { map: texture4 } );
+    const mesh10 = new THREE.Mesh( photo4,frame4, 10 );
+    mesh10.position.set(-15,80,-103);
+    scene.add( mesh10 );
         
-    // Grouping of trees
-    //const geometry = new THREE.ConeGeometry( 10, 60, 8, 1 );
-    //const material = new THREE.MeshPhongMaterial( { color: 0xd355fa, flatShading: true } );
-    //const mesh = new THREE.InstancedMesh( geometry, material, 500 );
-    //const tree = new THREE.Object3D();
-    //for ( let i = 0; i < 75; i ++ ) {
-    //    tree.position.x = Math.random() * 250 - 125;
-    //    tree.position.y = 0;
-    //    tree.position.z = Math.random() * 250 - 125;
-    //    tree.updateMatrix();
-    //    mesh.setMatrixAt( i, tree.matrix );
-    //}
-    //scene.add( mesh );
+    
 
     // Ground
     const earth = new THREE.PlaneGeometry( 2000, 2000 );

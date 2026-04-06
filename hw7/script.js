fd11e9fa794d2342af1f0afdfc4f097e852b9589
navinad-create18 <navina@vt.edu>
2026-04-06 11:30:28 -0400

    Finished hw7

--- hw7/script.js
+++ hw7/script.js
@@ -17,7 +17,8 @@ import { PointerLockControls } from './src/PointerLockControls.js';
 //The plug-in for Font Loader
 import { FontLoader } from './src/FontLoader.js';
 
-
+//The plug-in to load glb models
+import { GLTFLoader } from './src/GLTFLoader.js';
 
 // Declaring global variables.
 let camera, canvas, controls, scene, renderer;
@@ -34,6 +35,8 @@ let canJump = false;
 let prevTime = performance.now();
 const velocity = new THREE.Vector3();
 const direction = new THREE.Vector3();
+
+let assetsScene;
 
 // Run the "init" function which is like "setup" in p5.
 init();
@@ -55,7 +58,7 @@ function init() {
     // Setup camera
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
     camera.position.set( 0, 10, 0 );
-    camera.scale.set(2,2,2);
+    camera.scale.set(0.5,0.5,0.5);
 
     // Setup Orbit controls
     //controls = new OrbitControls( camera, renderer.domElement );
@@ -122,7 +125,7 @@ controls.lock();
             break;
 
         case 'Space':
-            if ( canJump === true ) velocity.y += 350;
+            if ( canJump === true ) velocity.y += 300;
             canJump = false;
             break;
 
@@ -165,78 +168,72 @@ controls.lock();
 
     //End of First person setup
 
+    //load assets
+    const loader = new GLTFLoader();
+        loader.load( './assets/Project2_scene.glb',  function ( gltf ) {
+
+            assetsScene = gltf.scene;
+            assetsScene.scale.set (0.5,0.5,0.5);
+            assetsScene.position.set(0,0,-60);
+            assetsScene.translateY(5);
+            scene.add( assetsScene ); 
+        },undefined, function ( e ) {
+
+            console.error( e );
+
+        } );
+      
+    
     //Add font and text
-    const loader = new FontLoader();
-    loader.load('./IosevkaCharon_Mono_Bold.json', function (font){
-        
-        //create color and material
-        const color = 0xd70a6a;
-        
-        const matDark = new THREE.LineBasicMaterial( {
-            color: color,
-            side: THREE.DoubleSide
-        } );
-        
-        const matLite = new THREE.MeshBasicMaterial( {
-            color: color,
-            transparent: true,
-            opacity: 0.4,
-            side: THREE.DoubleSide
-        } );
-        
-        // backslash \n creates a new line in your message
-        const message = 'Tuesday March 24 Demo\n Another line';
-        //shape and size of message
-        const shapes = font.generateShapes(message, 40);
-        const textGeometry = new THREE.ShapeGeometry( shapes );
-        
-        //creates bounding box around text like in Illustrator
-        textGeometry.computeBoundingBox();
-        
-        //translate to center like center align text
-        const xMid = - 0.5 * ( textGeometry.boundingBox.max.x - 
-        textGeometry.boundingBox.min.x );
-        textGeometry.translate( xMid, 0, 0 );
-        
-        //ad ojects to scene
-        const text = new THREE.Mesh ( textGeometry, matLite );
-        text.position.y = 50;
-        text.position.z = -200;
-        text.position.x = 50;
-        scene.add (text);
-        
-    } );
+    //const loader = new FontLoader();
+    //loader.load('./IosevkaCharon_Mono_Bold.json', function (font){
+    //    
+    //    //create color and material
+    //    const color = 0xd70a6a;
+    //    
+    //    const matDark = new THREE.LineBasicMaterial( {
+    //        color: color,
+    //        side: THREE.DoubleSide
+    //    } );
+    //    
+    //    const matLite = new THREE.MeshBasicMaterial( {
+    //        color: color,
+    //        transparent: true,
+    //        opacity: 0.4,
+    //        side: THREE.DoubleSide
+    //    } );
+    //    
+    //    // backslash \n creates a new line in your message
+    //    const message = 'Tuesday March 24 Demo\n Another line';
+    //    //shape and size of message
+    //    const shapes = font.generateShapes(message, 40);
+    //    const textGeometry = new THREE.ShapeGeometry( shapes );
+    //    
+    //    //creates bounding box around text like in Illustrator
+    //    textGeometry.computeBoundingBox();
+    //    
+    //    //translate to center like center align text
+    //    const xMid = - 0.5 * ( textGeometry.boundingBox.max.x - 
+    //    textGeometry.boundingBox.min.x );
+    //    textGeometry.translate( xMid, 0, 0 );
+    //    
+    //    //ad ojects to scene
+    //    const text = new THREE.Mesh ( textGeometry, matLite );
+    //    text.position.y = 50;
+    //    text.position.z = -200;
+    //    text.position.x = 50;
+    //    scene.add (text);
+    //    
+    //} );
     
     
     // Add world geometry
  //torus knot shape
     const coolShape = new THREE.TorusKnotGeometry( 10,3,100,16 );
     
-    //torus knot color
-    const knotColor = new THREE.MeshPhongMaterial ({color: 0x7f01a2});
-    const knotShape = new THREE.Mesh(coolShape, knotColor);
-    
-    knotShape.position.z = -300;
-    knotShape.position.y = 50;
-    scene.add( knotShape );
-   
-       //March 24 example material and object
-    const donut = new THREE.TorusGeometry( 50, 20, 16, 100 );
-    const donutMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-    
-    const torus = new THREE.Mesh( donut, donutMaterial );
-    torus.position.y =50;
-    torus.position.z = -250;
-    scene.add( torus );
-    
-    const donutLine = new THREE.MeshBasicMaterial ({color: 0x000000, wireframe:true});
-    const torusLine = new THREE.Mesh( donut, donutLine );
-    torusLine.position.y =50;
-    torusLine.position.z = -250;
-    scene.add( torusLine );
     
      //Grouping of trees
-    puffyTrees();
+    //puffyTrees();
     
     
     
@@ -246,7 +243,7 @@ controls.lock();
     const earth = new THREE.PlaneGeometry( 2000, 2000 );
     const ground = new THREE.MeshPhongMaterial( { color: 0xffdce5, flatShading: true } );
     const mesh2 = new THREE.InstancedMesh( earth, ground, 500 );
-    mesh2.translateY( -60 );
+    mesh2.translateY( 0 );
     mesh2.rotateX( -1.5708 );
     scene.add( mesh2 );
     
